ca96b87788a10e72fcacd8044d8995ac22452475
Ariful Islam <arif98741@users.noreply.github.com>
2018-07-05 07:41:07 +0600

    Newfeature (#1)
    Thanks for your interest to work with this repo

--- something.php
+++ something.php
@@ -1,2 +1,10 @@
 something is written here. Currently working at something.php
+
+anything can happend. I don't care about that.
+
 you should do rebase before sending pull request
+
+x = 4;
+y = 7;
+result = x+y
+
